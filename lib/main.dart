2252171bf9fa6e2323b6b5664cfd80975d11741c
Allanetizen <allankosgei747@gmail.com>
2020-06-02 13:02:20 +0300

    themes,wrapping long lines, animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,6 +6,17 @@ void main() {
     FlutterChatApp(),
   );
 }
+
+final ThemeData kIOSTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 String _name = 'Allan';
 
@@ -17,42 +29,54 @@ class FlutterChatApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Chat',
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: ChatScreen(),
     );
   }
 }
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
 
   final String text;
+  final AnimationController animationController;
 
   @override
   Widget build(BuildContext context) {
     Row();
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child: Text(_name[0])),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(_name,
-                  style:
-                      Theme.of(context).textTheme.headline), //check back here
-              Container(
-                margin: EdgeInsets.only(top: 5.0),
-                child: Text(text),
+    return SizeTransition(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(_name,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline), //check back here
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
     );
   }
 }
@@ -62,20 +86,30 @@ class ChatScreen extends StatefulWidget {
   _ChatScreenState createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
   final FocusNode _focusNode = FocusNode();
+  bool _isComposing = false;
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      // NEW
+      _isComposing = false; // NEW
+    });
     ChatMessage message = ChatMessage(
       text: text,
+      animationController: AnimationController(
+        duration: const Duration(milliseconds: 700),
+        vsync: this,
+      ),
     );
     setState(() {
       _messages.insert(0, message);
     });
     _focusNode.requestFocus();
+    message.animationController.forward();
   }
 
   @override
@@ -84,27 +118,47 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text('FlutterChat'),
         backgroundColor: Colors.deepPurple,
+        elevation: Theme
+            .of(context)
+            .platform == TargetPlatform.iOS ? 2.0 : 4.0,
       ),
-      body: Column(
-        children: [
-          Flexible(
-            child: ListView.builder(
-              // factory method builds a list on demand by providing a function that is called once per item in the list;
+      body: Container(
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  // factory method builds a list on demand by providing a function that is called once per item in the list;
 
-              padding: EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              Divider(
+                height: 1.0,
+                color: Colors.deepPurple,
+              ),
+              Container(
+                decoration: BoxDecoration(color: Theme
+                    .of(context)
+                    .cardColor),
+                child: _buildTextComposer(),
+              ),
+            ],
+          ),
+
+          decoration: Theme
+              .of(context)
+              .platform == TargetPlatform.iOS // NEW
+              ? BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey[200]), // NEW
             ),
-          ),
-          Divider(height: 1.0,
-          color: Colors.deepPurple,),
-          Container(
-            decoration: BoxDecoration(color: Theme.of(context).cardColor),
-            child: _buildTextComposer(),
-          ),
-        ],
-      ),
+          )
+              : null),
+
+
     );
   }
 
@@ -118,24 +172,40 @@ class _ChatScreenState extends State<ChatScreen> {
             Flexible(
               child: TextField(
                 controller: _textController,
+                onChanged: (String text) {
+                  // NEW
+                  setState(() {
+                    // NEW
+                    _isComposing = text.length > 0; // NEW
+                  }); // NEW
+                },
                 onSubmitted: _handleSubmitted,
                 decoration:
-                    InputDecoration.collapsed(hintText: 'Send a message'),
+                InputDecoration.collapsed(hintText: 'Send a message'),
                 focusNode: _focusNode,
               ),
             ),
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 4.0),
-              child: IconButton(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                child: IconButton(
                   icon: Icon(
                     Icons.send,
                     color: Colors.deepPurple,
                   ),
-                  onPressed: () => _handleSubmitted(_textController.text)),
-            ),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null,
+                )),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages)
+      message.animationController.dispose();
+    super.dispose();
   }
 }
